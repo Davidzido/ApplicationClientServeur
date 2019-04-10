d7a16853bc201244456a3bbd06067227bff6ea2d
David-Gabriel Verrette <33006091+Davidzido@users.noreply.github.com>
2019-04-10 00:04:42 -0400

    Update Client.java

--- Client.java
+++ Client.java
@@ -12,102 +12,131 @@ import java.io.*;
 
 public class Client {
 
+	HangmanClient hangmanClient = new HangmanClient();
+
 	private final static String ADDRESS = "127.0.0.1";
 	private final static int PORT = 5000;
 	private static Socket socket;
+	private static Scanner input;
 
 	private static boolean gameStarted;
 	private static boolean gameOver;
+	private static boolean checkGameStartedMessage;
+	private static boolean isDisplayShown;
+	private static boolean isPlayerTurn;
+
+	// THE BRAIN
+	public static Scanner getInput() throws IOException {
+		return input;
+	}
 
 	private static boolean isGameStarted() throws IOException {
-		Scanner input = new Scanner(socket.getInputStream());
-		String isGameStarted = input.nextLine();
-		return (isGameStarted.equalsIgnoreCase("true")) ? true : false;
+		//return getInput().hasNextBoolean() ? getInput().nextBoolean() : false;
+		System.out.println("Charles : ");
+		return Boolean.parseBoolean(getInput().nextLine());
+		//return getInput().nextBoolean();
 	}
 
 	public static boolean isPlayerTurn() throws IOException {
-		Scanner input = new Scanner(socket.getInputStream());
-		String playerTurn = input.nextLine();
-		System.out.println("IsPlayerTurn = " + playerTurn);
-		return (playerTurn.equalsIgnoreCase("true")) ? true : false;
+		return getInput().hasNextBoolean() ? getInput().nextBoolean() : false;
 	}
 
-	// Retourne le nombre de vies restantes
-	public static int nbVie() throws IOException {
-		Scanner input = new Scanner(socket.getInputStream());
-		int vie = input.nextInt();
-		return vie;
-	}
-
-	// Retourne les caractËres dÈj‡ essayÈs
-	public static String valeursEssayer() throws IOException {
-		Scanner input = new Scanner(socket.getInputStream());
-		String charUsed = input.nextLine();
-		return charUsed;
+	
+	public static String getDisplayValue() throws IOException {
+		return getInput().nextLine();
 	}
 
 	public static void display() throws IOException {
-		// On peut Ègalement faire en sorte que la mÈthode renvoie un String
-		// qui sera composÈ du bonhomme, des valeurs essayÈes, la solution partielle et
+		// On peut √©galement faire en sorte que la m√©thode renvoie un String
+		// qui sera compos√© du bonhomme, des valeurs essay√©es, la solution partielle et
 		// le nb de vies restantes
 
-		// Afficher le nombre de vies restantes
-		System.out.println("Vie(s): " + nbVie());
-
-		// input les valeurs essayer
-		System.out.println(valeursEssayer());
-
-		// Affichage de la solution partielle
-		// System.out.println(getSolutionPartielle());
-
+		String[] displaySplit = getDisplayValue().split(":");
+		int vie = Integer.parseInt(displaySplit[0]);
+		String charUsed = displaySplit[1];
+		String solutionPartielle = displaySplit[2];
+		
+		
+		// UPPERBORDER
+		System.out.println("*******************************************");
 		// Montrer le bonhomme
-		//System.out.println(getBonhomme(nbVie()));
+		System.out.println(HangmanClient.getBonhomme(vie));
+		// Montrer nbre de vie restante
+		System.out.println("Vie(s): " + vie);
+		// Montrer les valeurs essay√©es
+		System.out.println("C: "+charUsed);
+		// Montrer la solution partielle
+		System.out.println("R: " + solutionPartielle);
+		// LOWERBORDER
+		System.out.println("*******************************************");
+	}
 
+	public static void reponseHandler() throws IOException {
+		String reponse = getInput().nextLine();
+		System.out.println("Reponse = "+reponse);
+		if(reponse.equalsIgnoreCase("ATTENTION:")) {
+			System.out.println(reponse);
+			System.out.println(getInput().nextLine());
+		}
+		else if(reponse.equalsIgnoreCase("ATTENTION: La lettre a d√©ja √©t√© essay√©!") 
+				|| reponse.contains("Bravo") 
+				|| reponse.contains("Ouch")) {
+			System.out.println(reponse);
+			display();
+			isPlayerTurn = false;
+		}
 	}
 
 	public static void main(String args[]) {
-		int c = 0;
 		try {
 			socket = new Socket(ADDRESS, PORT);
 			// System.out.println("Enter lines of text :");
 			Scanner sc = new Scanner(System.in);
-			Scanner input = new Scanner(socket.getInputStream());
+			input = new Scanner(socket.getInputStream());
 			PrintWriter output = new PrintWriter(socket.getOutputStream(), true);
-			while (c < 10) {
 
-				// En attende du dÈbut de la partie.
+			// WELCOME MESSAGE
+
+			while (socket.isConnected()) {
+
+				// En attende du d√©but de la partie.
 				if (!gameStarted) {
-					System.out.println("En attente de dÈbut de partie.");
+					if (!checkGameStartedMessage) {
+						System.out.println("En attente de d√©but de partie.");
+						checkGameStartedMessage = true;
+					}
 					gameStarted = isGameStarted();
+					//System.out.println("gameStarted = "+gameStarted);
 					continue;
 				}
 
 				// Game Loop
 				while (gameStarted || !gameOver) {
-
 					// Player turn loop
-					while (isPlayerTurn()) {
-						// Montrer le GUI
-						display();
-
+					while (isPlayerTurn) {
+						if (!isDisplayShown) {
+							// Montrer le UI
+							getInput().nextLine();
+							display();
+							isDisplayShown = true;
+						}
 						// Demander une lettre
-						System.out.println("Veuillez choisir un caractËre : ");
-						output.println(sc.nextLine().charAt(0));
+						System.out.println("Veuillez choisir un caract√®re : ");
+						char userInput = sc.nextLine().charAt(0);
+						if (HangmanClient.isInputAcceptable(userInput)) {
+							output.println(userInput);
+						} else {
+							System.out.println("U dumb, plz tri agane");
+							continue;
+						}
 
-						// Montrer le GUI
-						display();
+						reponseHandler();
 					}
+					//System.out.println("Player turn " + isPlayerTurn);
+					
+					isPlayerTurn = isPlayerTurn();
 				}
 
-				System.out.println("Partie commencÈ");
-
-				// output.println(sc.nextLine());
-				// if (input.hasNextLine()) {
-				// System.out.println(input.nextLine());
-				// } else {
-				// break;
-				// }
-				c++;
 			}
 			sc.close();
 			input.close();
